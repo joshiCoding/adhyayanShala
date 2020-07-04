fc51bd085175cfc49d2904142376298343959412
joshiCoding <joshi.mohit1221@gmail.com>
2020-07-05 01:09:27 +0530

    added basic scroll unmodified

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,47 @@ menuCloseBtn.addEventListener('click', e=> {
     menu.classList.remove('nav-container-open');
 })
 
+
+//for scroll function on enquire button
+const enquireBtn = document.querySelector('.enquire');
+// const moveToClassInfo = document.querySelector('');
+console.log(enquireBtn);
+
+enquireBtn.addEventListener('click', e=>{
+    smoothScroll('info-get');
+});
+
+//copied from stackoverflow
+function smoothScroll(elementId){
+    var MIN_PIXELS_PER_STEP = 16;
+    var MAX_SCROLL_STEPS = 30;
+    var target = document.getElementById(elementId);
+    var scrollContainer = target;
+    do {
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do {
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    var pixelsPerStep = Math.max(MIN_PIXELS_PER_STEP,
+                                 (targetY - scrollContainer.scrollTop) / MAX_SCROLL_STEPS);
+
+    var stepFunc = function() {
+        scrollContainer.scrollTop =
+            Math.min(targetY, pixelsPerStep + scrollContainer.scrollTop);
+
+        if (scrollContainer.scrollTop >= targetY) {
+            return;
+        }
+
+        window.requestAnimationFrame(stepFunc);
+    };
+
+    window.requestAnimationFrame(stepFunc);
+}
